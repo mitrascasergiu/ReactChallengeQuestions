cb34e57f1555782b82e2d226657d774f6d2d3f42
mitrascasergiu <mitrasca.sergiu@gmail.com>
2023-05-14 17:40:37 +0300

    Add files via upload

---
+++ CustomerOnboarding/src/components/CustomerListTableSorting.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Table, Select } from 'antd';
+
+const { Option } = Select;
+
+const CustomerListTableSorting = ({ customers, updateOnboardingStatus }) => {
+  const handleChangeStatus = (customerId, status) => {
+    const customerIndex = customers.findIndex(
+      (customer) => customer.id === customerId
+    );
+    updateOnboardingStatus(customerIndex, status);
+  };
+
+  const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+        sorter: (a, b) => a.id - b.id,
+        sortDirections: ['ascend', 'descend'],
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Onboarding Status',
+      dataIndex: 'onboardingStatus',
+      key: 'onboardingStatus',
+      render: (status, record) => (
+        <Select
+          defaultValue={status}
+          onChange={(value) => handleChangeStatus(record.id, value)}
+        >
+          <Option value="inProgress">In Progress</Option>
+          <Option value="completed">Completed</Option>
+        </Select>
+      ),
+    },
+  ];
+
+  return (<Table dataSource={customers} columns={columns} pagination={false}/>);
+};
+
+export default CustomerListTableSorting;
